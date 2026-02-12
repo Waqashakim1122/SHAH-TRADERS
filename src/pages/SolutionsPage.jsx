68be62b7f72d90e2c888261f626bcd1595cf9f8f
Waqashakim1122 <162884901+Waqashakim1122@users.noreply.github.com>
2026-02-12 18:14:03 +0500

    Add PackingLabelingShowcase to SolutionsPage

--- src/pages/SolutionsPage.jsx
+++ src/pages/SolutionsPage.jsx
@@ -1,11 +1,11 @@
-import { SolutionsIntro, FulfillmentShowcase, TransportationShowcase, WarehousingShowcase } from '../components/showcases';
+import { SolutionsIntro, FulfillmentShowcase,  PackingLabelingShowcase, WarehousingShowcase } from '../components/showcases';
 
 export default function SolutionsPage({ setActiveModal }) {
   return (
     <main className="pt-24">
       <SolutionsIntro />
       <FulfillmentShowcase />
-      <TransportationShowcase />
+      <PackingLabelingShowcase />
       <WarehousingShowcase />
     </main>
   );
