4f29c532ec02f2cdbb98ee484a3de2b396e8dcd7
Waqashakim1122 <162884901+Waqashakim1122@users.noreply.github.com>
2026-02-12 18:12:04 +0500

    Add PackingLabelingShowcase export to index.js

--- src/components/showcases/index.js
+++ src/components/showcases/index.js
@@ -1,5 +1,7 @@
 // src/components/showcases/index.js
 export { default as SolutionsIntro } from './SolutionsIntro';
 export { default as FulfillmentShowcase } from './FulfillmentShowcase';
-export { default as TransportationShowcase } from './TransportationShowcase';
+export { default as PackingLabelingShowcase } from './PackingLabelingShowcase';
+
+
 export { default as WarehousingShowcase } from './WarehousingShowcase';
