a7f80996925b2907abc178d57da481dbde352373
Waqashakim1122 <162884901+Waqashakim1122@users.noreply.github.com>
2026-02-21 17:11:55 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
 // Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -21,10 +20,6 @@ import PricingSection from './components/PricingSection';
 import EcommerceFulfillmentDetailPage from './pages/EcommerceFulfillmentDetailPage';
 import WarehousingDetailPage from './pages/WarehousingDetailPage';
 import PackingLabelingDetailPage from './pages/PackingLabelingDetailPage';
-// Remove these if they don't exist yet:
-// import ColdChainPage from './pages/ColdChainPage';
-// import ProcessPage from './pages/ProcessPage';
-// import GlobalNetwork from './components/GlobalNetwork';
 
 export default function App() {
   const [activeModal, setActiveModal] = useState(null);
@@ -38,22 +33,15 @@ export default function App() {
           <Route path="/" element={<HomePage setActiveModal={setActiveModal} />} />
           <Route path="/calculator" element={<PricingSection />} />
           <Route path="/about" element={<AboutUs />} />
-         
           <Route path="/contact" element={<ContactUs />} />
           <Route path="/get-quote" element={<GetQuote />} />
           <Route path="/solutions" element={<SolutionsPage setActiveModal={setActiveModal} />} />
           <Route path="/solutions/fulfillment" element={<FulfillmentPage />} />
           <Route path="/solutions/PackingLabeling" element={<PackingLabelingPage />} />
           <Route path="/solutions/warehousing" element={<WarehousingPage />} />
-          // In your routes:
-<Route path="/solutions/ecommerce-fulfillment" element={<EcommerceFulfillmentDetailPage />} />
-<Route path="/solutions/ware-housing" element={<WarehousingDetailPage />} />
-<Route path="/solutions/packing-labeling" element={<PackingLabelingDetailPage />} />
-          
-          {/* Comment out routes for pages you haven't created yet */}
-          {/* <Route path="/network" element={<GlobalNetwork />} /> */}
-          {/* <Route path="/process" element={<ProcessPage />} /> */}
-          {/* <Route path="/solutions/cold-chain" element={<ColdChainPage />} /> */}
+          <Route path="/solutions/ecommerce-fulfillment" element={<EcommerceFulfillmentDetailPage />} />
+          <Route path="/solutions/ware-housing" element={<WarehousingDetailPage />} />
+          <Route path="/solutions/packing-labeling" element={<PackingLabelingDetailPage />} />
         </Routes>
 
         <Footer />
