f3333ca1a83f40defb3823cf2bc5ddff58a39d5d
Waqashakim1122 <162884901+Waqashakim1122@users.noreply.github.com>
2026-03-02 16:22:22 +0500

    Update FulfillmentShowcase.jsx

--- src/components/showcases/FulfillmentShowcase.jsx
+++ src/components/showcases/FulfillmentShowcase.jsx
@@ -152,7 +152,7 @@ function VideoOnlyCard({ videoSrc, posterImage }) {
         poster={posterImage}
         onLoadedData={handleLoadedData}
         onError={handleError}
-        preload="metadata"
+        preload="none"
       >
         <source src={videoSrc} type="video/mp4" />
         Your browser does not support the video tag.
@@ -161,5 +161,6 @@ function VideoOnlyCard({ videoSrc, posterImage }) {
     </motion.div>
   );
 }
+
 
 
